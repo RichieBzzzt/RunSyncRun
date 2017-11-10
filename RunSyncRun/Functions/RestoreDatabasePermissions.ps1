20c4a99c204759611847a9a1fe4bc78d761e5200
Unknown <richie@bzzzt.io>
2017-11-10 16:22:25 +0000

    staging missed file

--- RunSyncRun/Functions/RestoreDatabasePermissions.ps1
+++ RunSyncRun/Functions/RestoreDatabasePermissions.ps1
@@ -13,17 +13,36 @@ Function Restore-DatabasePermissions {
             Write-Host $permission.Process
             Write-Host $permission.Role.Name
             $restoredPermission = $null
-            if ($permission.Read -eq "Allowed")
-            {
+            if ($permission.Read -eq "Allowed") {
                 Write-Host "Adding read permissions to $($permission.Role.Name) on $ssasDatabase $ssasServer" -ForegroundColor Green
                 $restoredPermission = $exists.DatabasePermissions.Add($permission.Role.Name)
-                $restoredPermission
-                $restoredPermission.Update()
-                Write-Host "What is going on? "$restoredPermission.Role.Name
+                Write-Host  $restoredPermission.Read
+                Write-Host  $restoredPermission.Role.Name
                 $restoredPermission.Read = [Microsoft.AnalysisServices.ReadAccess]::Allowed
-                $restoredPermission.Update()
             }
+            if ($permission.Process -eq "Allowed") {
+                Write-Host "Adding Process permissions to $($permission.Role.Name) on $ssasDatabase $ssasServer" -ForegroundColor Green
+                $restoredPermission = $exists.DatabasePermissions.Add($permission.Role.Name)
+                Write-Host  $restoredPermission.Process
+                Write-Host  $restoredPermission.Role.Name
+                $restoredPermission.Process = [Microsoft.AnalysisServices.ReadAccess]::Allowed
+            }
+            if ($permission.Administer -eq "Allowed") {
+                Write-Host "Adding Administer permissions to $($permission.Role.Name) on $ssasDatabase $ssasServer" -ForegroundColor Green
+                $restoredPermission = $exists.DatabasePermissions.Add($permission.Role.Name)
+                Write-Host  $restoredPermission.Administer
+                Write-Host  $restoredPermission.Role.Name
+                $restoredPermission.Administer = [Microsoft.AnalysisServices.ReadAccess]::Allowed
+            }
+            $restoredPermission.Update()
+            Write-Host  $restoredPermission.Read
+            Write-Host  $restoredPermission.Role.Name
             $exists.Update()
+            Write-Host  $restoredPermission.Read
+            Write-Host  $restoredPermission.Role.Name
+            $ssasServer.Update()
+            Write-Host  $restoredPermission.Read
+            Write-Host  $restoredPermission.Role.Name
         }
     }
 }
